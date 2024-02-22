e51141ab3eed9f041465d370600304df50c31ca1
Reza Banankhah <banankha@usc.edu>
2024-02-22 00:33:45 -0800

    Added icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,5 +3,7 @@ import People from "./People.svelte";
 import Time from "./Time.svelte";
 import Trophy from "./Trophy.svelte";
 import Search from "./Search.svelte";
+import CheckMark from "./CheckMark.svelte";
+import X from "./X.svelte";
 
-export { Book, People, Time, Trophy, Search };
+export { Book, People, Time, Trophy, Search, CheckMark, X };
