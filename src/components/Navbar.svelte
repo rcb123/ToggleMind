d73d16864392bdc24b6123b2fb8fb07a6e45b821
Reza Banankhah <banankha@usc.edu>
2024-02-26 16:01:51 -0800

    Updated auth buttons
    Changed login and signup buttons to act as links but appear as buttons

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -6,7 +6,9 @@
     <div class="w-4 h-4">
       <!-- Future logo placeholder -->
     </div>
-    <h1 class="font-bold text-base md:text-lg tracking-tight">ToggleMind</h1>
+    <h1 class="font-bold text-base md:text-lg tracking-tight">
+      <a href="/">ToggleMind</a>
+    </h1>
   </div>
   <!-- Navigation and user actions -->
   <nav class="flex justify-end items-center gap-1 md:gap-2 lg:gap-8">
@@ -19,14 +21,16 @@
       <a href="/pricing">Pricing</a>
     </div>
     <!-- User actions -->
-    <div class="flex gap-1 md:gap-2 lg:gap-4 font-bold">
-      <button
-        class="py-2 w-16 md:w-20 bg-sky-500 text-white rounded-xl text-sm md:text-base"
-        >Sign up</button
+    <div class="flex gap-2 sm:gap-4 font-bold">
+      <a
+        href="/signup"
+        class="py-2 w-16 md:w-20 bg-sky-500 text-white rounded-xl text-sm text-center md:text-base"
+        >Sign up</a
       >
-      <button
-        class="py-2 w-16 md:w-20 bg-slate-200 text-black rounded-xl text-sm md:text-base"
-        >Log in</button
+      <a
+        href="/login"
+        class="py-2 w-16 md:w-20 bg-slate-200 text-black rounded-xl text-sm text-center md:text-base"
+        >Log in</a
       >
     </div>
   </nav>
